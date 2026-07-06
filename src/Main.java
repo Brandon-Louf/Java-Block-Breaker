9c76ed31ff44bd93ad4a0b4e5ab336d9e9d20147
brandon-louf <brandon.louf@gmail.com>
2026-07-06 12:21:02 -0600

    Created the JFrame window and attributes.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //The JFrame is the window in which the game is played and its attributes
+        JFrame object = new JFrame();
+        object.setBounds(10, 10, 700, 600);
+        object.setTitle("Breaker Ball");
+        object.setResizable(false);
+        object.setVisible(true);
+        object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
