20a0b1a82a8c81184d34e2894fe4c16d6a5f8107
brandon-louf <brandon.louf@gmail.com>
2026-07-07 16:41:25 -0600

    Added controls and started timer.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // The JFrame is the window in which the game is played and its attributes
         JFrame object = new JFrame();
+        Gameplay gamePlay = new Gameplay();
         object.setBounds(10, 10, 700, 600);
         object.setTitle("Breaker Ball");
         object.setResizable(false);
         object.setVisible(true);
         object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        object.add(gamePlay);
 
         // Adds the Gameplay class/properties to the window
         Gameplay gameStart = new Gameplay();
