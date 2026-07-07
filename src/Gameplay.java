3327e7cee0f51f4a58900aba21016b40275f4c9d
brandon-louf <brandon.louf@gmail.com>
2026-07-07 13:07:43 -0600

    Defined various gameplay variables and graphics.

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
 
 /* Extends from JPanel to inherit its properties and mesh with the window
 * Implements KeyListener and ActionListener for controls */
@@ -22,6 +24,35 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private int ballY = 350;
     private int ballXDirect = -1;
     private int ballYDirect = -2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        //time = new Timer(delay, this);
+        //time.start();
+    }
+
+    // Defines the graphics
+    public void paint(Graphics g) {
+        // Background
+        g.setColor(Color.BLUE);
+        g.fillRect(1, 1, 692, 592);
+
+        // Boarder
+        g.setColor(Color.GRAY);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        // Slider
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(sliderX, 550, 100, 8);
+
+        // Ball
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(ballX, ballY, 20, 20);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
