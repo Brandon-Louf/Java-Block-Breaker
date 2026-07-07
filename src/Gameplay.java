27544fcdbe95c4950fd83f44157187362c94119c
brandon-louf <brandon.louf@gmail.com>
2026-07-07 16:40:57 -0600

    Correctly added Gameplay to the main method.

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import javax.swing.Timer;
 
 /* Extends from JPanel to inherit its properties and mesh with the window
 * Implements KeyListener and ActionListener for controls */
@@ -29,8 +29,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        //time = new Timer(delay, this);
-        //time.start();
+        time = new Timer(delay, this);
+        time.start();
     }
 
     // Defines the graphics
@@ -51,12 +51,15 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
         // Ball
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(ballX, ballY, 20, 20);
+        g.fillOval(ballX, ballY, 20, 20);
+        g.dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        // Starts the timer and recalls the graphics
+        time.start();
+        repaint();
     }
 
     @Override
@@ -64,9 +67,37 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     }
 
+    // Controls for the paddle
     @Override
     public void keyPressed(KeyEvent e) {
+        // Left input
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (sliderX >= 600) {
+                // Prevents the slider from going off the panel
+                sliderX = 600;
+            } else {
+                moveLeft();
+            }
+        }
+        // Right input
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (sliderX < 10) {
+                // Prevents the slider from going off the panel
+                sliderX = 10;
+            } else {
+                moveRight();
+            }
+        }
+    }
 
+    // Movement methods called from the controls
+    public void moveLeft() {
+        play = true;
+        sliderX -= 20;
+    }
+    public void moveRight() {
+        play = true;
+        sliderX += 20;
     }
 
     @Override
