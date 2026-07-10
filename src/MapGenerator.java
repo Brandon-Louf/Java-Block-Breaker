cde44991790146f83ffef0ff1ef951088eb21b55
brandon-louf <brandon.louf@gmail.com>
2026-07-10 08:03:17 -0600

    Setup for block generation.

--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -1,11 +1,28 @@
+import java.awt.*;
+
 public class MapGenerator {
+    // Setup for an array grid of bricks
     public int map[][];
     public int brickWidth;
     public int brickHeight;
     public MapGenerator(int row, int col) {
         map = new int[row][col];
         for (int i = 0; i < map.length; i++) {
-
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = 1;
+            }
+        }
+        brickWidth = 540/col;
+        brickHeight = 150/row;
+    }
+    public void draw(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(Color.YELLOW);
+                    g.fillRect(j * brickWidth + 88, i * brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
         }
     }
 }
